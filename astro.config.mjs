cd33d147b4f3bee2421dd4f967d9f66ccbf79eaa
DorrD <dorrd2002@gmail.com>
2024-03-07 19:37:41 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,23 +6,20 @@ import vercel from "@astrojs/vercel/static";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.meinscord.com",
-  integrations: [
-    mdx({
-      syntaxHighlight: "shiki",
-      shikiConfig: { theme: "github-dark-dimmed" },
-      gfm: true,
-    }),
-    icon(),
-    sitemap(),
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
-  adapter: vercel({
-    analytics: true,
-  }),
+  integrations: [mdx({
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "github-dark-dimmed"
+    },
+    gfm: true
+  }), icon(), sitemap(), react(), tailwind({
+    applyBaseStyles: false
+  })],
+  adapter: vercel(),
+  output: "server"
 });
